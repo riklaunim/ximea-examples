233f373e1eead542bb40a6e8b6693379d4c492f2
riklaunim <riklaunim@gmail.com>
2021-02-28 13:04:14 +0100

    Connect base signals.

--- pyqt/run.py
+++ pyqt/run.py
@@ -10,6 +10,18 @@ class Recorder(QtWidgets.QMainWindow, viewer.Ui_xCapture):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self.exposureSlider.sliderMoved.connect(self._exposure_changed)
+        self.gainSlider.sliderMoved.connect(self._gain_changed)
+        self.frameCounter.valueChanged.connect(self._frame_count_changed)
+
+    def _exposure_changed(self, exposure):
+        self.exposureValue.setText(str(exposure))
+
+    def _gain_changed(self, gain):
+        self.gainValue.setText(str(gain))
+
+    def _frame_count_changed(self, frame_count):
+        self.captureValue.setText(str(frame_count))
 
 
 def main():
